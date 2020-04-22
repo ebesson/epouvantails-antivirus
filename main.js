55edaa55c4a5820ce3b72031ea414c44a00ec341
ebesson <etienne.besson@gmail.com>
2020-04-22 21:17:56 +0200

    feat: add scarecrows

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const Scarecrow = class {
+
+    constructor(coordinates, src) {
+      this.coordinates = coordinates;
+      this.src = src;
+    }
+
+    gpsCoordinates() {
+      return this.coordinates;
+    }
+    image() {
+        return this.src;
+    }
+  }
+  
+
 var customOptions =  {    
     'maxWidth': String.valueOf((screen.width/2)-5),
     'width': '200',
@@ -25,36 +41,38 @@ function createMap(elemId, centerLat, centerLng, zoom) {
     return map;
 }
 
-function addMarker(map, latLng, onClick) {
-    var photoImg = "https://static.pexels.com/photos/189349/pexels-photo-189349.jpeg";
-    var marker = L.marker(latLng).addTo(map);
-    marker.bindPopup('<img  style="max-height:'+ (screen.height-70) +'px;max-width:'+ (screen.width-70) +'px;" src="' + photoImg + '"/>', customOptions).openPopup();
-    marker.on('click', function(e){
-        map.setView([e.latlng.lat, e.latlng.lng]);
-    });
+function addMarker(map, scarecrow) {
+    
+    var marker = L.marker(scarecrow.gpsCoordinates()).addTo(map);
+    if (scarecrow.image() != ""){
+        marker.bindPopup('<img  style="max-height:'+ (screen.height-70) +'px;max-width:'+ (screen.width-70) +'px;" src="' + scarecrow.image() + '"/>', customOptions).openPopup();
+        marker.on('click', function(e){
+            map.setView([e.latlng.lat, e.latlng.lng]);
+        });
+    }
     return marker;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     var map = createMap('map', 48.1238, -1.8618, 16);
 
-    markersLatLng = [
-        [48.12486, -1.86563], 
-        [48.12593, -1.86686],
-        [48.11959, -1.86407],
-        [48.12168, -1.85930], 
-        [48.12494, -1.86160], 
-        [48.11957, -1.86368], 
-        [48.12521, -1.86760], 
-        [48.12361, -1.86780], 
-        [48.12670, -1.86169], 
-        [48.12365, -1.86146],
-        [48.12317, -1.86279],
-        [48.11931, -1.86210],
+    var scarecrows = [
+        new Scarecrow([48.12486, -1.86563], "images/000.png"), 
+        new Scarecrow([48.12593, -1.86686], "images/001.png"),
+        new Scarecrow([48.11959, -1.86407], "images/002.png"),
+        new Scarecrow([48.12168, -1.85930], "images/003.png"), 
+        new Scarecrow([48.12494, -1.86160], "images/004.png"), 
+        new Scarecrow([48.11957, -1.86368], "images/005.png"), 
+        new Scarecrow([48.12521, -1.86760], ""), 
+        new Scarecrow([48.12361, -1.86780], "images/007.png"), 
+        new Scarecrow([48.12670, -1.86169], "images/008.png"), 
+        new Scarecrow([48.12365, -1.86146], "images/009.png"),
+        
+        new Scarecrow([48.11931, -1.86210], "images/011.png"),
     ];
 
-    markersLatLng.forEach(function(latLng) {
-        addMarker(map, latLng);
+    scarecrows.forEach(function(scarecrow) {
+        addMarker(map, scarecrow);
     });
 
 });
